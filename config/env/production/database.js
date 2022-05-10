a12226db58432d937c7892d0d3026cbed9e2384d
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-05-10 17:41:03 +0530

    db---> postgres

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,18 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false }
-        },
-        options: {
-          ssl: false
-        },
+const parse = require('pg-connection-string').parse;
+const config = parse('postgres://lshjxbjeilzmgg:44865908c31e788f1ddf34cf63cb8ed3ebebe89d0b7b2ebeece8b9a4e889ad6a@ec2-54-158-247-210.compute-1.amazonaws.com:5432/d8a5e7c2utc49j');
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
     },
-  };
-};
+    debug: false,
+  },
+});

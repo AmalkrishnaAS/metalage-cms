d56b8331d130d1a2ac9f38d575bb5b8c394be844
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-05-23 20:19:49 +0530

    categories

--- src/api/section/services/section.js
+++ src/api/section/services/section.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category service.
+ * section service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::category.category');
+module.exports = createCoreService('api::section.section');

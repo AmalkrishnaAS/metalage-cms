2259165fbb1c822f6c1d15e44291a9d9bac5fb40
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-08-05 00:32:17 +0530

    hello

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,26 @@
 module.exports = [
   "strapi::errors",
   {
-    
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-            directives: {
-              'connect-src': ["'self'", 'https:'],
-              'img-src': ["'self'", 'data:', 'blob:', `metalages.s3.ap-south-1.amazonaws.com`],
-              'media-src': ["'self'", 'data:', 'blob:', `metalages.s3.ap-south-1.amazonaws.com`],
-              upgradeInsecureRequests: null,
-          },
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "metalages.s3.ap-south-1.amazonaws.com"],
+          "media-src": ["'self'", "data:", "blob:", "metalages.s3.ap-south-1.amazonaws.com"],
+          upgradeInsecureRequests: null,
         },
       },
     },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
 ];
  
  
